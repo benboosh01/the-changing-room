c0ae6dca5acc8ab6d477a6895a36bc19ed8a9790
Ben Bushell <benbushell@Bens-Air.lan>
2022-10-10 13:54:29 +0100

    starter template

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
 <template>
   <div class="container" style="padding: 50px 0 100px 0">
-    <Profile v-if="store.user" />
-    <Auth v-else />
+    <Title />
   </div>
 </template>
 
 <script>
-import { store } from "./store"
-import { supabase } from "./supabase"
-import Auth from "./components/Auth.vue"
-import Profile from "./components/Profile.vue"
+import Title from './components/Title.vue'
 export default {
   components: {
-    Auth,
-    Profile,
-  },
-
-  setup() {
-    store.user = supabase.auth.user()
-    supabase.auth.onAuthStateChange((_, session) => {
-      store.user = session.user
-    })
-
-    return {
-      store,
-    }
+    Title
   },
 }
 </script>
