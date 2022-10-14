61c5c6c9f9a2e484bd2517d3b64df17639d51f7e
Ben Bushell <ben.bushell87@icloud.com>
2022-10-14 17:01:04 +0100

    fix swap form

--- src/components/SwapForm.vue
+++ src/components/SwapForm.vue
@@ -1,18 +1,17 @@
 <template>
-    <main v-show="!submitted"><h3>
-        Ready to swap with {{ username }}? Complete the from below to submit your
-        swap request
+  <main v-show="!submitted">
+    <h3>
+      Ready to swap with {{ username }}? Complete the from below to submit your
+      swap request
     </h3>
-    <p>
-        Hi {{ username }}, I'd like your item. Here's what I can
-        swap:
-    </p>
+    <p>Hi {{ username }}, I'd like your item. Here's what I can swap:</p>
     <label htmlFor="swapForm">Choose which item you want to offer</label>
     <select name="swapForm" v-model="selectedItem">
-        <option v-for="item in userItems">{{ item.item_name }}</option>
+      <option v-for="item in userItems">{{ item.item_name }}</option>
     </select>
-    <button @click="createNewSwap(selectedItem)">Request swap</button></main>
- 
+    <button @click="createNewSwap(selectedItem)">Request swap</button>
+  </main>
+
   <p v-show="submitted">
     Success! {{ username }} will now review your request and approve it if
     they're happy to go ahead
@@ -20,13 +19,13 @@
 </template>
 
 <script>
-import { onMounted, ref } from "vue";
-import { useStore } from "../store";
-import { supabase } from "../supabase";
+import { onMounted, ref } from 'vue';
+import { useStore } from '../store';
+import { supabase } from '../supabase';
 
 export default {
-  name: "SwapForm",
-  props: ["username", "userId", "itemId", "itemName"],
+  name: 'SwapForm',
+  props: ['username', 'userId', 'itemId', 'itemName'],
   setup(props) {
     const store = useStore();
     const loggedInUser = store.user.id;
@@ -34,7 +33,7 @@ export default {
     const requestedItemId = props.itemId;
     const userItems = ref([]);
     const loading = ref(false);
-    const selectedItemId = ref("");
+    const selectedItemId = ref('');
     const submitted = ref(false);
 
     async function getUserItems() {
@@ -42,9 +41,9 @@ export default {
         loading.value = true;
 
         const { data, error } = await supabase
-          .from("items")
-          .select("item_name, id")
-          .eq("owner_id", loggedInUser);
+          .from('items')
+          .select('item_name, id')
+          .eq('owner_id', loggedInUser);
 
         if (data) {
           userItems.value = data;
@@ -73,12 +72,12 @@ export default {
         user_to: loggedInUser,
         item_id: requestedItemId,
       };
-      console.log(swapOne)
-        console.log(swapTwo)
+      console.log(swapOne);
+      console.log(swapTwo);
 
       try {
-        const { error } = await supabase.from("swaps").insert(swapOne);
-        const { error2 } = await supabase.from("swaps").insert(swapTwo);
+        const { error } = await supabase.from('swaps').insert(swapOne);
+        const { error2 } = await supabase.from('swaps').insert(swapTwo);
         if (error || error2) throw error;
       } catch (error) {
         alert(error.message);
@@ -99,7 +98,6 @@ export default {
       createNewSwap,
       selectedItemId,
       submitted,
-      itemName,
     };
   },
 };
--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -3,10 +3,12 @@
   <Profile />
   <UserItems />
   <SwapsList />
+  <MessageInbox />
 </template>
 
 <script setup>
 import Profile from '../components/Profile.vue';
 import SwapsList from '../components/SwapsList.vue';
 import UserItems from '../components/UserItems.vue';
+import MessageInbox from '../components/MessageInbox.vue';
 </script>
