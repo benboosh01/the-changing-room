23299d9675a28f87fe6be71d8cdbba6ca662deb2
Ben Bushell <ben.bushell87@icloud.com>
2022-10-14 11:51:39 +0100

    add sign out functionality

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="container" style="padding: 50px 0 100px 0">
     <TheNavigation />
+    <button v-if="store.user.id" @click="logOut">Log Out</button>
     <Title />
     <router-view></router-view>
   </div>
 </template>
 
-<script>
-import Title from './components/Title.vue'
-import TheNavigation from './components/TheNavigation.vue'
+<script setup>
+import Title from './components/Title.vue';
+import TheNavigation from './components/TheNavigation.vue';
+import { store } from './store';
+import router from './router';
 
-export default {
-  components: {
-    Title,
-    TheNavigation,
-},
+function logOut() {
+  if (store.user.id) {
+    store.user = {};
+    router.push({ path: '/home' });
+  }
 }
 </script>
