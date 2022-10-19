6adc4d198d81ddfc202fe44d515e3aeb6b4542a4
Daniel-N98 <Danieldnee@hotmail.com>
2022-10-19 10:11:13 +0100

    Initial change to show all incoming & outgoing messages

--- src/components/MessageInbox.vue
+++ src/components/MessageInbox.vue
@@ -1,16 +1,16 @@
 <template>
   <section style="text-align: center">
     <h2>My messages</h2>
-    <li v-for="item in messages" :key="item.from + store.user.id">
+    <li v-for="item in messages" :key="item.from[0] + item.from[1]">
       From: {{ item.from_username }}
-      <button @click="showChat" :value="item.from + store.user.id">
+      <button @click="showChat" :value="item.from[0] + item.from[1]">
         {{ userClicked(item) ? "Hide " : "Show " }}chat
       </button>
       <IndividualMessageBox v-if="userClicked(item)" :messages="item" />
       <MessageForm
         v-if="userClicked(item)"
         :username="item.from_username"
-        :userId="item.from"
+        :userId="item.from[0]"
       />
     </li>
   </section>
@@ -37,7 +37,7 @@ export default {
     }
 
     function userClicked(item) {
-      return show.value && itemId.value === item.from + store.user.id;
+      return show.value && itemId.value === item.from[0] + item.from[1];
     }
 
     async function getUserMessages() {
@@ -46,12 +46,15 @@ export default {
         const { data: items, error } = await supabase
           .from("messages")
           .select("*")
-          .eq("user_to_id", store.user.id);
+          .or(
+            `user_to_id.eq.${store.user.id},user_from_id.eq.${store.user.id}`
+          );
         if (error) throw error;
-        messages.value.push(...groupMessages(items));
+        console.log("done items");
+        messages.value.push(...(await groupMessages(items)));
         messages.value.map(
           async (item) =>
-            (item.from_username = await getUsernameFromUUID(item.from))
+            (item.from_username = await getUsernameFromUUID(item.from[0]))
         );
       } catch (error) {
         alert(error.message);
@@ -69,7 +72,7 @@ export default {
         if (error) throw error;
         return data.username;
       } catch (error) {
-        alert(error);
+        alert(error.message);
       }
     }
 
@@ -79,29 +82,47 @@ export default {
      * @param {array} messages
      * @returns {array}
      */
-    function groupMessages(messages) {
+    async function groupMessages(messages) {
       const groups = [];
-      messages.forEach((message) => {
+      for (let message of messages) {
         const user_from_id = message.user_from_id;
+        const from_username = await getUsernameFromUUID(user_from_id);
+        if (
+          user_from_id === store.user.id &&
+          message.user_to_id === store.user.id
+        ) {
+          continue;
+        }
         const messageObj = {
+          from: from_username,
           message: message.text,
           created_at: new Date(message.timestamp),
         };
         let changed = false;
-        groups.forEach((group) => {
-          if (group["from"] === user_from_id) {
+        for (let group of groups) {
+          if (
+            group.from.length === 2 &&
+            group.from.includes(message.user_from_id) &&
+            group.from.includes(message.user_to_id)
+          ) {
             group.messages.push(messageObj);
             changed = true;
-            return;
+            break;
           }
-        });
+        }
         if (!changed) {
           groups.push({
-            from: user_from_id,
+            from: [
+              store.user.id === message.user_from_id
+                ? message.user_to_id
+                : message.user_from_id,
+              store.user.id,
+            ],
             messages: [messageObj],
           });
         }
-      });
+      }
+      console.log("done groups");
       return groups;
     }
 
